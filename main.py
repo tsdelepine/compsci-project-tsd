eada2ec062cbea0decc54945804a3d8e5030be25
Toby Delepine <tsdelepine@seu.edu>
2026-04-23 15:50:00 -0400

    fixed file saving issues to allow highscore to be created and saved

--- main.py
+++ main.py
@@ -77,12 +77,12 @@ class Machine:
 def load_high_score():
     try:
         with open("highscore.txt", "r") as f:
-            return int(f.read)
+            return int(f.read())
     except:
         return 0
     
 def save_high_score(score):
-    with open("highscore.txt", "r") as f:
+    with open("highscore.txt", "w") as f:
         f.write(str(score))
 
 
